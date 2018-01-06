f683b930dfd51ab2f676dc62bfa6d7f7afb3e4c4
orionweblab <orionweblab@gmail.com>
2018-01-06 17:21:30 -0500

    Main folder files, management stuff

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+	<head>
+		<title>file uploader</title>
+		<link rel="icon" href="favicon.ico" type="image/x-icon"/>
+		<style>
+		#main{
+			background-color: #23272a;
+		}
+		#cen{
+			border-radius: 25px;
+			background: #2C2F33;
+			padding: 20px; 
+			width: 30%;
+			height: 15%; 
+			margin: auto;
+			margin-top: 15%;
+			background-repeat: no-repeat;
+			background-position: center; 
+			text-align: center;
+			color: #ffffff;
+			font-family: Arial;
+			font-size: 100%;
+		}
+		a:link {
+			color: skyblue;
+		}
+
+		a:visited {
+			color: skyblue;
+		}
+
+		a:hover {
+			color: limegreen;
+		}
+
+		a:active {
+			color: limegreen;
+		}
+		</style>
+	</head>
+	<body id="main">
+		<div id="cen">
+		<br/>
+			<h1>file uploader</h1>
+			<p>upload stuff: <a href="./upload/">here</a></p>
+		</div>
+	</body>
+</html>
